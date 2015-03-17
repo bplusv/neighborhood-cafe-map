3112fc95148f06a10baf9260110d6c0c223926b0
Luis Salazar <bp.lusv@gmail.com>
2015-03-16 20:00:38 -0600

    Add first test to pull place data from foursquare api

--- js/app.js
+++ js/app.js
@@ -2,13 +2,11 @@ var ViewModel = function() {
   var self = this;
 
   self.map = new google.maps.Map(document.getElementById('map-canvas'), {
-    zoom: 4,
-    center: new google.maps.LatLng(-25.363882, 131.044922)
+    zoom: 13,
+    center: new google.maps.LatLng(31.738019, -106.46183)
   });
 
-  self.infoWindow = new google.maps.InfoWindow({
-      content: '<p>this is a test</p>'
-  });
+  window.infoWindow = self.infoWindow = new google.maps.InfoWindow();
 
   self.places = ko.observableArray([]);
   self.search = ko.observable('');
@@ -44,12 +42,9 @@ var ViewModel = function() {
               map: self.map,
               title: place.name
           });
+
           google.maps.event.addListener(place.marker, 'click', function() {
-            self.infoWindow.open(self.map, place.marker);
-            place.marker.setAnimation(google.maps.Animation.BOUNCE);
-            setTimeout(function() {
-              place.marker.setAnimation(null);
-            }, 700);
+            self.showPlace(place);
           });
         });
 
@@ -58,13 +53,42 @@ var ViewModel = function() {
   };
 
   self.showPlace = function(place) {
-    $('.navmenu.canvas-slid').offcanvas('hide');
-
+    self.loadFoursquare();
     self.infoWindow.open(self.map, place.marker);
     place.marker.setAnimation(google.maps.Animation.BOUNCE);
     setTimeout(function() {
       place.marker.setAnimation(null);
     }, 700);
+  };
+
+  self.loadFoursquare = function() {
+    var url = 'https://api.foursquare.com/v2/venues/502707c7e4b0fe423d9d34e2?client_id=GYEK0PESSPOVGTN1AYR0XFPTHGPVGZDICA1ELEUZQ0M3MUFR&client_secret=JGKE1IPGETB11KPSWTP05HKMY0SF3ZQ4ENPIFD1HYVPY0JVF&v=20130815';
+    return $.getJSON(url).then(function(data) {
+        var place = {};
+
+        place.name = data.response.venue.name;
+        place.url = data.response.venue.shortUrl;
+        place.rating = data.response.venue.rating;
+        place.address = data.response.venue.location.formattedAddress[0];
+        place.likes = data.response.venue.likes.count;
+        place.photo = data.response.venue.bestPhoto.prefix;
+        place.photo += '100x150';
+        place.photo += data.response.venue.bestPhoto.suffix;
+
+        var content = '<p>' + place.name + '</p>';
+        content += '<p>' + place.rating + '</p>';
+        content += '<p>' + place.likes + '</p>';
+        content += '<p>' + place.address + '</p>';
+        content += '<p> <a href="' + place.url + '" target="_blank">Click here</a> </p>';
+        content += '<img src="' + place.photo + '" />';
+
+        self.infoWindow.setContent(content);
+    });
+  };
+
+  self.selectPlaceFromList = function(place) {
+    $('.navmenu.canvas-slid').offcanvas('hide');
+    self.showPlace(place);
   };
 };
 
